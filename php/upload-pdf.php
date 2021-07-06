b3e9ef4e99ed632a61d5b70bb4e3e85492a1a9c2
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2021-07-06 13:31:39 -0400

    Add files via upload
    Version 0.02

--- php/upload-pdf.php
+++ php/upload-pdf.php
@@ -1,11 +1,31 @@
 <?php
-$targetfolder = "var/www/pdf.spectrox.ca/testupload/";
-$targetfolder = $targetfolder . basename( $_FILES['file']['name']);
+if(isset($_FILES['file'])){
+    $file = $_FILES['file'];
+    
+    $file_name = $file['name'];
+    $file_tmp = $file['tmp_name'];
+    $file_size = $file['size'];
+    $file_error = $file['error'];
+    $file_ext = explode('.', $file_name);
+    $file_ext = strtolower(end($file_ext));
 
-if(move_uploaded_file($_FILES['file']['tmp_name'], $targetfolder)){
-    chmod($targetfolder, 0777);
-    echo "The file ". basename( $_FILES['file']['name']). " is uploaded";
-}else {
-echo "There has been an error uploading your PDF file. Please try again.";
+    $allowed_file_types = array('pdf', 'png', 'jpg');
+
+    if(in_array($file_ext, $allowed_file_types)){
+        if($file_error === 0){
+            if($file_size < 999999999){
+                $file_name_new = uniqid('', true) . '.' . $file_ext;
+                $file_destination = 'testupload/' . $file_name_new;
+
+                if(move_uploaded_file($file_tmp, $file_destination)){
+                    $file_destination;
+                }else{
+                    echo "There was an error uploading your file!\n";
+                    echo "<br>";
+                    echo "<br>";
+                    print_r(error_get_last());
+                }
+            }
+        }
+    }
 }
-?>
