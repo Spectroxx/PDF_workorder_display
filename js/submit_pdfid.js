6fa62f838fe5e9e34597e0277438412d0bafb2f6
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2021-07-07 14:01:46 -0400

    Commit Version 0.03

---
+++ js/submit_pdfid.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('pdfid_submit').click(function(){
+        $.post("upload-pdf.php",{
+            pdfid: $('#pdfid').val()
+        });
+    });
+
+});
--- php/upload-pdf.php
+++ php/upload-pdf.php
@@ -1,23 +1,30 @@
 <?php
+
+$pdfid = $_POST['pdfid'];
+echo "$pdfid";
+
+
+
 if(isset($_FILES['file'])){
     $file = $_FILES['file'];
-    
     $file_name = $file['name'];
     $file_tmp = $file['tmp_name'];
     $file_size = $file['size'];
     $file_error = $file['error'];
     $file_ext = explode('.', $file_name);
     $file_ext = strtolower(end($file_ext));
-
     $allowed_file_types = array('pdf', 'png', 'jpg');
 
     if(in_array($file_ext, $allowed_file_types)){
         if($file_error === 0){
             if($file_size < 999999999){
-                $file_name_new = uniqid('', true) . '.' . $file_ext;
-                $file_destination = 'testupload/' . $file_name_new;
+                echo var_dump($pdfid) . "<br>";
+                $file_name_new = rename($file_name, $pdfid) . '.' . $file_ext;
+                echo $file_destination = '/var/www/pdf.spectrox.ca/files/' . $file_name_new;
 
                 if(move_uploaded_file($file_tmp, $file_destination)){
+                    #header('refresh: 1; url= redirrect');
+                    echo '<script>alert("Success! Your file has been uploaded!")</script>';
                     $file_destination;
                 }else{
                     echo "There was an error uploading your file!\n";
@@ -29,3 +36,4 @@ if(isset($_FILES['file'])){
         }
     }
 }
+?>
