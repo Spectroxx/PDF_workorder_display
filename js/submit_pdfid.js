7daa2f45c3e2cbdda1dcb5a57d8a7c799ac2683d
Spectrox <67061496+Spectroxx@users.noreply.github.com>
2021-07-07 16:09:46 -0400

    update
    still nothing

--- js/submit_pdfid.js
+++ js/submit_pdfid.js
@@ -1,3 +1,4 @@
+/*
 $(document).ready(function(){
     $('pdfid_submit').click(function(){
         $.post("upload-pdf.php",{
@@ -5,4 +6,21 @@ $(document).ready(function(){
         });
     });
 
+});
+*/
+$(document).ready(function () {
+    $('#pdfid_submit').on('submit', function(e) {
+        e.preventDefault();
+        $.ajax({
+            url : $(this).attr('action') || window.location.pathname,
+            type: "GET",
+            data: $(this).serialize(),
+            success: function (data) {
+                $("#pdfid").html(data);
+            },
+            error: function (jXHR, textStatus, errorThrown) {
+                alert(errorThrown);
+            }
+        });
+    });
 });
