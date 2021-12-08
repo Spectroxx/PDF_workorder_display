fbe3045d3476822712029feb835f4a4736e869d7
Spectrox <julienmwestman@gmail.com>
2021-12-08 13:00:22 -0500

    2.20
    Fully functional updates

--- js/embed_pdf.js
+++ js/embed_pdf.js
@@ -1,6 +1,6 @@
 try{
   var ul = $('ul.pdf-embed');
-  var count = 25;
+  var count = 20;
   for(var i=1; i<=count; i++){
     ul.append('<embed src="resources/files/'+i+'.pdf#zoom=125,0,160&toolbar=0&scrollbar=0" type="application/pdf" width="18%" height="40%"<embed/>');  
   } 
