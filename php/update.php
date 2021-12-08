0810230c5042e4998abf0bcfa04e4bbcb5b41f5d
Spectrox <julienmwestman@gmail.com>
2021-12-08 12:49:32 -0500

    2.19
    almost there

--- php/update.php
+++ php/update.php
@@ -14,5 +14,7 @@ if(isset($_POST['update_files'])){
     exec('sh /home2/glavin/pdf.glavin.net/shell/update.sh', $output);
     print_r($output);
 }
+sleep(3);
+header("Location: https://pdf.glavin.net");
 
 ?>
