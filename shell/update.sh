8bcdbbcaa0a38e25a81456f0724494861046a596
Spectrox <julienmwestman@gmail.com>
2021-12-08 10:37:02 -0500

    2.15

--- shell/update.sh
+++ shell/update.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #Updates PDF Display
 
-cd tmp/ && git clone https://github.com/Spectroxx/pdf-display.git && rsync -av pdf-display/* ~/pdf.glavin.net/ && cd
+cd tmp/
+git clone https://github.com/Spectroxx/pdf-display.git
+rsync -av pdf-display/* ~/pdf.glavin.net/ 
+cd
