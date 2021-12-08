6fa26c3c907ca1f31db8a1deae8f156dd71fc589
Spectrox <julienmwestman@gmail.com>
2021-12-08 12:53:31 -0500

    2.19-B
    TEST

--- php/update.php
+++ php/update.php
@@ -13,6 +13,8 @@ $output=null;
 if(isset($_POST['update_files'])){
     exec('sh /home2/glavin/pdf.glavin.net/shell/update.sh', $output);
     print_r($output);
+    sleep(3);
+    header("Location: https://pdf.glavin.net");
 }
 
 ?>
