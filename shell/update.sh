ef641ca28bcebaec6c80a49a581b6f02bd098c10
Spectrox <julienmwestman@gmail.com>
2021-12-08 12:04:11 -0500

    2.18

--- shell/update.sh
+++ shell/update.sh
@@ -5,5 +5,4 @@ cd tmp/
 git clone https://github.com/Spectroxx/pdf-display.git
 rsync -a --exclude={'pdf-display/resources/*','pdf-display/README.md','pdf-display/LICENSE','pdf-display/.git/'} pdf-display/* ~/pdf.glavin.net/
 #rm -rf tmp/pdf-display/
-#test ligma
 cd
