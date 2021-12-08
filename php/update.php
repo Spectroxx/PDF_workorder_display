798d7fcf2d16f816759c6c6329f1c33074ce4e97
Spectrox <julienmwestman@gmail.com>
2021-12-08 06:49:50 -0500

    2.08
    lol oops

--- php/update.php
+++ php/update.php
@@ -8,10 +8,10 @@
  */
 
 
-if(isset($_Post['update_files']){
+if(isset($_Post['update_files'])){
     update();
 }
 function update() {
-    shell_exec('cd tmp/ && git clone https://github.com/Spectroxx/pdf-display.git && mv --update --force pdf-display/* ~/pdf.glavin.net/')
+    shell_exec('cd tmp/ && git clone https://github.com/Spectroxx/pdf-display.git && mv --update --force pdf-display/* ~/pdf.glavin.net/');
 }
 ?>
