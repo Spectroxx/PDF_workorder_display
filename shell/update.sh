7d7ae8041bf891c25e2baf36b42109984be3dfa1
Spectrox <julienmwestman@gmail.com>
2021-12-08 12:12:25 -0500

    2.18-A
    TEST

--- shell/update.sh
+++ shell/update.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 #Updates PDF Display
 
-cd tmp/
-git clone https://github.com/Spectroxx/pdf-display.git
-rsync -a --exclude={'pdf-display/resources/*','pdf-display/README.md','pdf-display/LICENSE','pdf-display/.git/'} pdf-display/* ~/pdf.glavin.net/
+git clone https://github.com/Spectroxx/pdf-display.git ~/pdf.glavin.net/tmp
+rsync -a --exclude={'pdf-display/resources/*','pdf-display/README.md','pdf-display/LICENSE','pdf-display/.git/'} ~/pdf.glavin.net/tmp/pdf-display/* ~/pdf.glavin.net/
 #rm -rf tmp/pdf-display/
-cd
+#TEST
