34263675acd0e62390ca4c7a20de5d7917fde109
Spectrox <julienmwestman@gmail.com>
2021-12-08 13:38:49 -0500

    D-2.21
    Dev Branch

--- shell/update.sh
+++ shell/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Updates PDF Display
+#Updates PDF Display [DEV BRANCH]
 
 git clone https://github.com/Spectroxx/pdf-display.git temp-pdf-display
 rsync -a --exclude={'pdf-display/resources/*','pdf-display/README.md','pdf-display/LICENSE','pdf-display/.git/'} temp-pdf-display/* ~/pdf.glavin.net/
